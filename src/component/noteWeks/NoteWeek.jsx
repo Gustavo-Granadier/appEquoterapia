970d8961b659eef6f2fb17fe7ec2da7e54e79ed3
Gustavo-Granadier <139660911+Gustavo-Granadier@users.noreply.github.com>
2025-09-17 17:35:33 -0300

    novo3

--- src/component/noteWeks/NoteWeek.jsx
+++ src/component/noteWeks/NoteWeek.jsx
@@ -32,7 +32,7 @@ const NoteWeek = () => {
 
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
                 <h2>Anotação Salva!</h2>
-                <p className='noteWeekDescription'>Sua anotação foi salva com sucesso. Qualquer informação é de extrema importacia para
+                <p>Sua anotação foi salva com sucesso. Qualquer informação é de extrema importacia para
                     o bom andamento do tratamento.
                 </p>
             </Modal>
